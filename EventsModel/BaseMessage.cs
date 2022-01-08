eb790dae7113f6be0171eaa0ab729e2d4248e9d8
Mikhail Kalatchev <kalatchev@hotmail.com>
2022-01-08 21:54:45 +0200

    Add project files.

---
+++ EventsModel/BaseMessage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skyware.Lis.EventsModel
+{
+
+    //Base class for all messages produced by LIS iLab
+    public abstract class BaseMessage
+    {
+
+        /// <summary>
+        /// Id of the message, GUID
+        /// </summary>
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        /// <summary>
+        /// Date and time when message is generated
+        /// </summary>
+        public DateTime EventTime { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// Origina application neme
+        /// </summary>
+        public string Application { get; set; }
+
+        /// <summary>
+        /// Origina applicaion version
+        /// </summary>
+        public string ApplicationVersion { get; set; }
+
+        /// <summary>
+        /// User Id (ussually username) of the logged in operator
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// If LIS supports location, Id of a location where event is raised
+        /// </summary>
+        public string LocationId { get; set; }
+
+        /// <summary>
+        /// Name of the location when event is raised
+        /// </summary>
+        public string LocationName { get; set; }
+
+        /// <summary>
+        /// Host name of a computer where message is generated (ussually it's NetBIOS name)
+        /// </summary>
+        public string HostName { get; set; }
+
+
+    }
+
+}
---
+++ EventsModel/InstrumentSampleQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skyware.Lis.EventsModel
+{
+
+    /// <summary>
+    /// Instrument makes query for a test selection by SampleId (barcode)
+    /// </summary>
+    public class InstrumentSampleQuery : BaseMessage
+    {
+
+        /// <summary>
+        /// Id of the instrument which makes query as it is defined in target LIS
+        /// </summary>
+        public string InstrumentId { get; set; }
+
+        /// <summary>
+        /// Name of the instrument which makes query as it is defined in target LIS
+        /// </summary>
+        public string InstrumentName { get; set; }
+
+        /// <summary>
+        /// SampleId (barcode) of a tube or vessel
+        /// </summary>
+        public string SampleId { get; set; }
+
+        /// <summary>
+        /// Disk or rack identifier where sample is found
+        /// </summary>
+        public string DiskOrRackId { get; set; }
+
+        /// <summary>
+        /// Position in the disk/rack of the sample
+        /// </summary>
+        public string DiskOrRackPosition { get; set; }
+
+        /// <summary>
+        /// Sequence number or any other identifier by which sample is known in the quering instrument
+        /// </summary>
+        public string InstrumentSampleId { get; set; }
+
+
+    }
+
+}
