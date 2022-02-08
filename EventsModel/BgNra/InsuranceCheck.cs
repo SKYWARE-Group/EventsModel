4eb7ca2c25e18b2bfd56e0a630beb859d0f78b7f
Mikhail Kalatchev <kalatchev@hotmail.com>
2022-02-08 02:13:35 +0200

    ver. 0.0.4

---
+++ EventsModel/BgNra/InsuranceCheck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skyware.Lis.EventsModel.BgNra
+{
+
+    /// <summary>
+    /// Occurs when Health Insurance (Bulgaria, Nra) is checked (topic://bg-nra/insurance)
+    /// </summary>
+    public class InsuranceCheck : BaseMessage
+    {
+
+        public override string DefaultAddress => "topic://bg-nra/insurance";
+
+        /// <summary>
+        /// Visit to which the patient is associated in thi check
+        /// </summary>
+        public int VisitId { get; set; }
+
+        /// <summary>
+        /// Patient identifier type, according to iLab
+        /// </summary>
+        public int PidType { get; set; }
+
+        /// <summary>
+        /// Patient identifier
+        /// </summary>
+        public string Pid { get; set; }
+
+        /// <summary>
+        /// Zero means insured, any non zero values indicates problem with insurance
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// Textual representation of a status
+        /// </summary>
+        public string StatusText { get; set; }
+
+        /// <summary>
+        /// Base64 encoded answer from NRZ
+        /// </summary>
+        public string OriginalFile { get; set; }
+
+
+    }
+
+}
